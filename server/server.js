c920829b142fb1ac265d575b7efa5d8dfe6e470a
Ward Cunningham <ward@c2.com>
2024-06-12 05:24:24 -0700

    server-side retrieve of single item

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 // mech plugin, server-side component
 // These handlers are launched with the wiki server.
 
+
 (function() {
+
+  const fs = require('fs')
+
   function startServer(params) {
     var app = params.app,
         argv = params.argv
 
-    return app.get('/plugin/mech/:thing', (req, res) => {
-      var thing = req.params.thing;
-      return res.json({thing});
+    return app.get('/plugin/mech/run/:slug([a-z-]+)/:itemId', (req, res) => {
+      console.log(req.params)
+      try {
+        const slug = req.params.slug
+        const itemId = req.params.itemId
+        const args = JSON.parse(atob(req.query.args || 'W10='))
+        const path = `${argv.db}/${slug}`
+        fs.readFile(path,(err,data) => {
+          const page = JSON.parse(data)
+          const item = page.story.find(item => item.id == itemId) || null
+          return res.json({err,item,args});
+        })
+      } catch(err) {
+        return res.json({err:err.message})
+      }
     })
+
   }
 
   module.exports = {startServer}
